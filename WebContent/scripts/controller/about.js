f18fbb5e6fd3e7e9c8bac4c3778f0190fd9ad742
sumankantmainali <sumankantmainali@gmail.com>
2015-07-23 12:15:10 -0700

    final fokder to work

---
+++ WebContent/scripts/controller/about.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dhagaThreadingApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dhagaThreadingApp
+ */
+angular.module('dhagaThreadingApp')
+  .controller('AboutCtrl', function ($scope) {
+
+
+
+
+
+
+  });
---
+++ WebContent/scripts/controller/services.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dhagaThreadingApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dhagaThreadingApp
+ */
+
+    angular.module('dhagaThreadingApp')
+ .controller('servicesCtrl', function ($scope,DhagaService) {
+    $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Karma',
+
+    ];
+
+
+  });
