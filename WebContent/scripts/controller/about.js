9f03fcc919ffde7f60c6432d6b4dadb1b1d2fa58
sumankantmainali <sumankantmainali@gmail.com>
2015-08-02 10:29:13 -0700

    update from suman

--- WebContent/scripts/controller/about.js
+++ WebContent/scripts/controller/about.js
@@ -16,3 +16,23 @@ angular.module('dhagaThreadingApp')
 
 
   });
+
+
+        $(document).ready(function() {
+            // Show or hide the sticky footer button
+            $(window).scroll(function() {
+                if ($(this).scrollTop() > 200) {
+                    $('.go-top').fadeIn(200);
+                } else {
+                    $('.go-top').fadeOut(200);
+                }
+            });
+
+            // Animate the scroll to top
+            $('.go-top').click(function(event) {
+                event.preventDefault();
+
+                $('html, body').animate({scrollTop: 0}, 300);
+            })
+        });
+
